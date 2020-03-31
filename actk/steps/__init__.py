7ebe1e32cb47631f9d57bd02664af7dec23ad44d
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-03-31 13:02:11 -0700

    Add 2d cell image step with parameterization

--- actk/steps/__init__.py
+++ actk/steps/__init__.py
@@ -3,5 +3,6 @@
 from .single_cell_features import SingleCellFeatures
 from .single_cell_image_3d import SingleCellImage3D
 from .standardize_fov_array import StandardizeFOVArray
+from .single_cell_image_2d import SingleCellImage2D
 
-__all__ = ["SingleCellFeatures", "StandardizeFOVArray", "SingleCellImage3D"]
+__all__ = ["SingleCellFeatures", "StandardizeFOVArray", "SingleCellImage3D", "SingleCellImage2D"]
---
+++ actk/steps/single_cell_image_2d/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .single_cell_image_2d import SingleCellImage2D  # noqa: F401
+
+__all__ = ["SingleCellImage2D"]
