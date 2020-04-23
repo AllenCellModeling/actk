a0b95680a16bfda6c5b164e741ebd849ee9b4933
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-04-23 13:14:45 -0700

    Rename columns to fields in missing data exception

--- actk/exceptions.py
+++ actk/exceptions.py
@@ -11,19 +11,19 @@ import pandas as pd
 
 class MissingDataError(Exception):
     def __init__(
-        self, dataset: Union[pd.DataFrame, dd.DataFrame], required_columns: List[str]
+        self, dataset: Union[pd.DataFrame, dd.DataFrame], required_fields: List[str]
     ):
         # Run base exception init
         super().__init__()
 
         # Store params for display
         self.dataset = dataset
-        self.required_columns = required_columns
+        self.required_fields = required_fields
 
     def __str__(self):
         return (
             f"Dataset provided does not have the required columns for this operation. "
             f"Dataset columns: {self.dataset.columns} "
-            f"Required columns: {self.required_columns} "
+            f"Required fields: {self.required_fields} "
             f"Dataset head: {self.dataset.head()}"
         )
