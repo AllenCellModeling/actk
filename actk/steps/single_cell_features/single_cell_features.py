5a041ea85d9ed44f1967219c0a3a9104080b4f67
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-06-04 11:58:39 -0700

    Drop batch size until ulimit is increased on SLURM nodes

--- actk/steps/single_cell_features/single_cell_features.py
+++ actk/steps/single_cell_features/single_cell_features.py
@@ -176,7 +176,7 @@ class SingleCellFeatures(Step):
                 [cell_ceiling_adjustment for i in range(len(dataset))],
                 [features_dir for i in range(len(dataset))],
                 [overwrite for i in range(len(dataset))],
-                batch_size=60,
+                batch_size=40,
             )
 
         # Generate features paths rows
