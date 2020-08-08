25997712916f9ada599084358f30b87d02c5a578
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2020-08-07 23:31:56 -0700

    fixed flake8 long line error

--- actk/steps/__init__.py
+++ actk/steps/__init__.py
@@ -5,4 +5,9 @@ from .single_cell_images import SingleCellImages
 from .standardize_fov_array import StandardizeFOVArray
 from .make_diagnostic_sheet import MakeDiagnosticSheet
 
-__all__ = ["SingleCellFeatures", "StandardizeFOVArray", "SingleCellImages", "MakeDiagnosticSheet"]
+__all__ = [
+    "SingleCellFeatures",
+    "StandardizeFOVArray",
+    "SingleCellImages",
+    "MakeDiagnosticSheet",
+]
