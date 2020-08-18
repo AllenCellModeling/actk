18900b157d08a9885d6ede9f6ca63bc71057886e
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-08-18 13:36:57 -0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -103,6 +103,6 @@ setup(
     url="https://github.com/AllenCellModeling/actk",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
