35bf162589a7dc6a5b0a02a8878f832afd5dde80
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-08-19 13:48:38 -0700

    Fix the order of dataset checking and dataset loading

--- actk/steps/single_cell_features/single_cell_features.py
+++ actk/steps/single_cell_features/single_cell_features.py
@@ -164,13 +164,13 @@ class SingleCellFeatures(Step):
         if isinstance(dataset, (str, Path)):
             dataset = Path(dataset).expanduser().resolve(strict=True)
 
-            # Check dataset and manifest have required fields
-            dataset_utils.check_required_fields(
-                dataset=dataset, required_fields=REQUIRED_DATASET_FIELDS,
-            )
+            # Read dataset
+            dataset = pd.read_csv(dataset)
 
-        # Read dataset
-        dataset = pd.read_csv(dataset)
+        # Check dataset and manifest have required fields
+        dataset_utils.check_required_fields(
+            dataset=dataset, required_fields=REQUIRED_DATASET_FIELDS,
+        )
 
         # Create features directory
         features_dir = self.step_local_staging_dir / "cell_features"
