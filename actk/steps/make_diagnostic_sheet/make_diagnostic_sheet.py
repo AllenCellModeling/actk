be1a80bb29b7be47704fdd553fbe739c213831ac
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2020-08-25 16:45:09 -0700

    removed flatten function util

--- actk/steps/make_diagnostic_sheet/make_diagnostic_sheet.py
+++ actk/steps/make_diagnostic_sheet/make_diagnostic_sheet.py
@@ -43,16 +43,6 @@ class DiagnosticSheetError(NamedTuple):
     error: str
 
 
-def flatten(x):
-    result = []
-    for el in x:
-        if hasattr(el, "__iter__") and not isinstance(el, str):
-            result.extend(flatten(el))
-        else:
-            result.append(el)
-    return result
-
-
 ###############################################################################
 
 
